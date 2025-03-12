dd3e85eb13a0573b7ab6853e627931e4660cab50
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-03-12 22:36:23 +0100

    add: constuctor from json file

--- src/main/java/tasktracker/model/TaskList.java
+++ src/main/java/tasktracker/model/TaskList.java
@@ -1,7 +1,13 @@
 package tasktracker.model;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import tasktracker.utils.JSONUtils;
 
 public class TaskList {
 
@@ -10,6 +16,18 @@ public class TaskList {
 	public TaskList() {
 		this.list = new ArrayList<>();
 	}
+
+	public TaskList(File json) {
+		JSONArray jArray = JSONUtils.getArrayFromFile(json);
+		if (jArray.length() > 0) {
+			for (int i = 0; i < jArray.length(); i++) {
+				JSONObject jOby = jArray.getJSONObject(i);
+				Task task = new Task(jOby);
+				this.list.add(task);
+			}
+		}
+	}
+
 	public void addTask(Task task) {
 		this.list.add(task);
 	}
