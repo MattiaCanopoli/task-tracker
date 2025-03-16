77d8e6699cc7dc594fb657cf619ea319674f8fde
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-03-16 17:24:21 +0100

    feat: now TaskList extends ArrayList
    fixed methods accordingly to new type

--- src/main/java/tasktracker/model/TaskList.java
+++ src/main/java/tasktracker/model/TaskList.java
@@ -1,23 +1,21 @@
 package tasktracker.model;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import tasktracker.utils.JSONUtils;
 
-public class TaskList {
+public class TaskList<T extends Task> extends ArrayList<Task> {
 
-	private List<Task> list;
-
-	public TaskList() {
-		this.list = new ArrayList<>();
-	}
+	// private List<Task> list;
+	//
+	// public TaskList() {
+	// this.list = new ArrayList<>();
+	// }
 
 	public TaskList(String filePath) {
-		this.list = new ArrayList<Task>();
 
 		JSONArray jArray = JSONUtils.getArrayFromFile(filePath);
 
@@ -25,17 +23,13 @@ public class TaskList {
 
 			JSONObject jOby = jArray.getJSONObject(i);
 			Task task = new Task(jOby);
-			this.list.add(task);
+			this.add(task);
 
 		}
 	}
 
-	public void addTask(Task task) {
-		this.list.add(task);
-	}
-
 	public Task getTaskByID(int ID) {
-		for (Task t : this.list) {
+		for (Task t : this) {
 			if (t.getId() == ID) {
 				return t;
 			}
@@ -47,10 +41,10 @@ public class TaskList {
 	public String toString() {
 		String taskList = "";
 
-		if (this.list.isEmpty()) {
+		if (this.isEmpty()) {
 			taskList = Color.RED_BOLD.toString() + "No task found";
 		}
-		for (Task t : this.list) {
+		for (Task t : this) {
 			taskList += t.toString() + "\n";
 		}
 
