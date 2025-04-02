d09251bd121504d83bfba586077aa1a231553628
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-04-02 17:12:40 +0200

    fix: updateJSON method now takes generic <T extends Task> as argument

--- src/main/java/tasktracker/utils/JSONUtils.java
+++ src/main/java/tasktracker/utils/JSONUtils.java
@@ -69,11 +69,25 @@ public class JSONUtils {
 			e.printStackTrace();
 		}
 	}
-	public static void updateJSON(TaskList<Task> list, String path) {
+	// public static void updateJSON(TaskList<Task> list, String path) {
+	//
+	// JSONArray jArray = new JSONArray();
+	//
+	// for (Task t : list) {
+	// JSONObject jObj = t.toJSON();
+	// jArray.put(jObj);
+	// }
+	//
+	// File file = new File(path);
+	//
+	// writeJSONtoFile(file, jArray);
+	// }
 
+	public static <T extends Task> void updateJSON(TaskList<T> taskList,
+			String path) {
 		JSONArray jArray = new JSONArray();
 
-		for (Task t : list) {
+		for (Task t : taskList) {
 			JSONObject jObj = t.toJSON();
 			jArray.put(jObj);
 		}
